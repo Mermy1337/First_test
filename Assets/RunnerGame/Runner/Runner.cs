9d1403f0b14a4243f18dfb9e8e2c192d84e0ae78
Drydd <drydd@users.noreply.github.com>
2014-09-07 17:13:23 +0200

    Ignore +Runner

---
+++ Assets/RunnerGame/Runner/Runner.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class Runner : MonoBehaviour {
+
+
+	#region Public Variables
+	public static float distanceTraveled;
+	public float accleration;
+	public Vector3 jumpVelocity;
+	public Vector3 speed;
+	#endregion
+
+	#region Private Variable
+	private bool touchingPlatform;
+	#endregion
+	
+	#region Methoden
+	void Update () {
+		if(touchingPlatform && Input.GetKeyDown(KeyCode.Space)){
+			rigidbody.AddForce(jumpVelocity,ForceMode.VelocityChange);
+			touchingPlatform = false;
+		}
+		if(Input.GetKeyDown(KeyCode.A)){
+			rigidbody.AddForce(-1f,0f,0f,ForceMode.VelocityChange);
+
+		}
+		if(Input.GetButton("Horizontal")){
+			//rigidbody.AddForce(1f,0f,0f,ForceMode.VelocityChange);
+			rigidbody.MovePosition(rigidbody.position +  Input.GetAxis("Horizontal") * speed * Time.deltaTime);
+
+		}
+		distanceTraveled = transform.localPosition.x;
+	
+	}
+
+	/*
+	void FixedUpdate(){
+		if(touchingPlatform)
+		{
+			rigidbody.AddForce(accleration,0f,0f,ForceMode.Acceleration);
+		}
+	} */
+
+	void OnCollisionEnter () {
+		touchingPlatform = true;
+	}
+	
+	void OnCollisionExit () {
+		touchingPlatform = false;
+	}
+	#endregion
+}
