4c30a167c88fc1fb56fef2cf9e9972fe4204745c
Drydd <drydd@users.noreply.github.com>
2014-09-08 19:29:23 +0200

    Started adding a Test Planet

---
+++ Assets/PlanetGen/Scripts/CreatePlanet.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class CreatePlanet : MonoBehaviour {
+
+	public int planetSize;
+	public Vector3 startPosition;
+	public float blockSize, minY, maxY;
+
+	public Sprite[] sprites;
+	
+	private Vector3 nextPosition;
+
+	void Start () {
+
+		GameObject go = new GameObject("Planet");
+		go.transform.localPosition = startPosition;
+		go.transform.localScale = new Vector2(blockSize,blockSize);
+		SpriteRenderer renderer = go.AddComponent<SpriteRenderer>();
+		SphereTestScript pRotation = go.AddComponent<SphereTestScript>();
+		pRotation.RotationSpeed = 5f;
+		renderer.sprite = sprites[0];
+	}
+
+
+	void Update () {
+
+	}
+	
+
+}
---
+++ Assets/PlanetGen/Scripts/SphereTestScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereTestScript : MonoBehaviour {
+
+	#region Public Variables
+	public  float RotationSpeed;
+	#endregion
+	
+	#region Methoden
+	void Update () {
+		//transform.Translate(5f * Time.deltaTime, 0f, 0f);
+		transform.Rotate(0, 0, RotationSpeed*  Time.deltaTime, Space.World);
+		
+	}
+	#endregion
+}
