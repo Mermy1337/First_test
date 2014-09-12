375f3eac57113dab15e0a39d66e7782341ce5daa
Mermy1337 <spacefighter2@gmx.de>
2014-09-12 17:07:27 +0200

    * fixed movement

--- Assets/Scripts/PlayerLogic.cs
+++ Assets/Scripts/PlayerLogic.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class PlayerLogic : MonoBehaviour {
 
-	// Use this for initialization
+	public float PlayerSpeed = 1;
 
 	public Animator anim;
+
+	// Use this for initialization
+
 	void Start () {
 		anim = gameObject.GetComponent<Animator> ();
 		Debug.Log("hello world");
@@ -16,13 +19,8 @@ public class PlayerLogic : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if (Input.GetKey(KeyCode.UpArrow)) {
-			rigidbody2D.AddForce(new Vector2(0,0.32f));
-		}
-		else if (Input.GetKey(KeyCode.DownArrow)) {
-			rigidbody2D.AddForce(new Vector2(0,-0.32f));
-			anim.SetTrigger("TurnDown");
-		}
-	
+		// just a simple movement. see http://docs.unity3d.com/ScriptReference/Input.GetAxis.html
+		transform.Translate(Input.GetAxis("Horizontal") * PlayerSpeed * Time.deltaTime,0,0);
+		transform.Translate(0,Input.GetAxis("Vertical") * PlayerSpeed * Time.deltaTime,0);
 	}
 }
