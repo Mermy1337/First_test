9a9746f5defb6ec061bee1936fd46fb180b26ff6
Drydd <drydd@users.noreply.github.com>
2014-09-12 13:39:22 +0200

    Little Changes

---
+++ Assets/Scripts/FPSHud.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FPSHud : MonoBehaviour {
+
+	public  float updateInterval = 0.5F;
+	
+	private float accum   = 0; // FPS accumulated over the interval
+	private int   frames  = 0; // Frames drawn over the interval
+	private float timeleft; // Left time for current interval
+
+
+	// Use this for initialization
+	void Start () {
+
+		if( !guiText )
+		{
+			Debug.Log("UtilityFramesPerSecond needs a GUIText component!");
+			enabled = false;
+			return;
+		}
+		timeleft = updateInterval; 
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		timeleft -= Time.deltaTime;
+		accum += Time.timeScale/Time.deltaTime;
+		++frames;
+		
+		// Interval ended - update GUI text and start new interval
+		if( timeleft <= 0.0 )
+		{
+			// display two fractional digits (f2 format)
+			float fps = accum/frames;
+			string format = System.String.Format("{0:F2} FPS",fps);
+			guiText.text = format;
+			
+			if(fps < 30)
+				guiText.material.color = Color.yellow;
+			else 
+				if(fps < 10)
+					guiText.material.color = Color.red;
+			else
+				guiText.material.color = Color.green;
+			//	DebugConsole.Log(format,level);
+			timeleft = updateInterval;
+			accum = 0.0F;
+			frames = 0;
+		}
+	
+	}
+
+	void onGUI()
+	{
+		GUI.TextArea(new Rect(0,0,Screen.width / 2,10),guiText.text);
+	}
+}
