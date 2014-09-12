be4376814a7db07feb774e4cd23286db745ace70
Mermy1337 <spacefighter2@gmx.de>
2014-09-12 15:30:33 +0200

    * set the create_world to a dynamic on

--- Assets/Scripts/Dungeon.cs
+++ Assets/Scripts/Dungeon.cs
@@ -7,8 +7,13 @@ public class Dungeon : MonoBehaviour {
 
 	private SpriteRenderer spriteRenderer;
 	private List<char> world;
-	private Vector2 position = new Vector2(-0.32f * 6.5f, 0.32f * 6.5f);
+	private Vector2 position = new Vector2(0, 0);
 	private GameObject walls, floors, dungeon;
+
+	// width und height in tiles
+	private int DungeonHeight = 50;
+	private int DungeonWidth = 50;
+
 
 
 	// Use this for initialization
@@ -22,60 +27,43 @@ public class Dungeon : MonoBehaviour {
 		floors.transform.parent = dungeon.transform;
 
 		world = new List<char>();
-		for(int y = 0; y <= 14; y++){
-			for( int x = 0; x <= 14; x++)
+		for(int y = 0; y <= DungeonHeight; y++){
+			for( int x = 0; x <= DungeonWidth; x++)
 			{
-				if(y == 0 || y == 14 || x == 0 || x == 14 )
+				if(y == 0 || y == DungeonHeight || x == 0 || x == DungeonWidth )
 				{
 					world.Add('x');
+					GameObject wall = new GameObject("Wall" );
+					wall.transform.parent = walls.transform;
+					wall.transform.position = position;
+					wall.AddComponent<SpriteRenderer>();
+					wall.GetComponent<SpriteRenderer>().sprite = sprites[0];
+					wall.AddComponent<BoxCollider2D>();
 				}
 
 				else 
 				{
 					world.Add('o');
+
+					GameObject floor  = new GameObject("Floor");
+					floor.transform.parent = floors.transform;
+					floor.transform.position = position;
+					floor.AddComponent<SpriteRenderer>();
+					floor.GetComponent<SpriteRenderer>().sprite = sprites[1];
 				}
 
-			
-			}
-
-
-		}
-
-
-		foreach( char x in world)
-		{
-			if (x == 'x')
-			{
-				GameObject wall = new GameObject("Wall" );
-				wall.transform.parent = walls.transform;
-				wall.transform.position = position;
-				wall.AddComponent<SpriteRenderer>();
-				wall.GetComponent<SpriteRenderer>().sprite = sprites[0];
-				wall.AddComponent<BoxCollider2D>();
-			}
-			else if (x == 'o'){
-				GameObject floor  = new GameObject("Floor");
-				floor.transform.parent = floors.transform;
-				floor.transform.position = position;
-				floor.AddComponent<SpriteRenderer>();
-				floor.GetComponent<SpriteRenderer>().sprite = sprites[1];
-			}
-
-			if( position.x >= 0.32f * 6.5f	)
-			{
-				position.x = -0.32f * 6.5f;
-				position.y += -0.32f;
-			}
-			else{
 				position.x += 0.32f;
 			}
 
-		} 
-
+			position.y += 0.32f;
+			position.x = 0.00f;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
 	}
+
+	
 }
