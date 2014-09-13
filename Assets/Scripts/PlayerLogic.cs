989b5c7b8bdfffc5ac858f19f79114b1a07d3646
Mermy1337 <spacefighter2@gmx.de>
2014-09-13 11:45:04 +0200

    * added animation to the movement

--- Assets/Scripts/PlayerLogic.cs
+++ Assets/Scripts/PlayerLogic.cs
@@ -4,14 +4,17 @@ using System.Collections;
 public class PlayerLogic : MonoBehaviour {
 
 	public float PlayerSpeed = 1;
+	protected Animator anim;
 
-	public Animator anim;
+		
 
 	// Use this for initialization
 
 	void Start () {
-		anim = gameObject.GetComponent<Animator> ();
 		Debug.Log("hello world");
+
+		// Get the Animator component from your gameObject
+		anim = GetComponent<Animator>();
 	}
 
 
@@ -22,5 +25,27 @@ public class PlayerLogic : MonoBehaviour {
 		// just a simple movement. see http://docs.unity3d.com/ScriptReference/Input.GetAxis.html
 		transform.Translate(Input.GetAxis("Horizontal") * PlayerSpeed * Time.deltaTime,0,0);
 		transform.Translate(0,Input.GetAxis("Vertical") * PlayerSpeed * Time.deltaTime,0);
+
+
+		/* Player Direction for animating. must be improved!:
+			0 = idle
+			1 = right
+			2 = down
+			3 = left
+			4 = top
+		*/
+		if (Input.GetAxis ("Horizontal") > 0) {
+			anim.SetInteger("PlayerDirection", 1);
+		} else if(Input.GetAxis ("Horizontal") < 0){
+			anim.SetInteger("PlayerDirection", 3);
+		} else if (Input.GetAxis ("Vertical") > 0) {
+			anim.SetInteger("PlayerDirection", 4);
+		} else if(Input.GetAxis ("Vertical") < 0){
+			anim.SetInteger("PlayerDirection", 2);
+		} else {
+			anim.SetInteger("PlayerDirection", 0);
+		}
+
+
 	}
 }
