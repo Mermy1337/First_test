635f97e51cd2e3b22481f6d36b3725e5f8e21b38
drydd <denisweil@gmx.de>
2014-10-18 15:26:31 +0200

    Some Changes
    Changed Player into Cube and added Jumping

--- Assets/Scripts/PlayerLogic.cs
+++ Assets/Scripts/PlayerLogic.cs
@@ -4,17 +4,22 @@ using System.Collections;
 public class PlayerLogic : MonoBehaviour {
 
 	public float PlayerSpeed = 1;
+	public  int jumpHigh = 100;
+
 	protected Animator anim;
+
+	private bool touchingPlatform;
+
 
 		
 
 	// Use this for initialization
 
 	void Start () {
-		Debug.Log("hello world");
+		//Debug.Log("hello world");
 
 		// Get the Animator component from your gameObject
-		anim = GetComponent<Animator>();
+		//anim = GetComponent<Animator>();
 	}
 
 
@@ -23,10 +28,7 @@ public class PlayerLogic : MonoBehaviour {
 	void Update () {
 
 		// just a simple movement. see http://docs.unity3d.com/ScriptReference/Input.GetAxis.html
-		transform.Translate(Input.GetAxis("Horizontal") * PlayerSpeed * Time.deltaTime,0,0);
-		//transform.Translate(0,Input.GetAxis("Vertical") * PlayerSpeed * Time.deltaTime,0);
 
-		transform.Translate(0,0,Input.GetAxis("Vertical") * PlayerSpeed * Time.deltaTime);
 
 
 		/* Player Direction for animating. must be improved!:
@@ -36,6 +38,7 @@ public class PlayerLogic : MonoBehaviour {
 			3 = left
 			4 = top
 		*/
+		/*
 		if (Input.GetAxis ("Horizontal") > 0) {
 			anim.SetInteger("PlayerDirection", 1);
 		} else if(Input.GetAxis ("Horizontal") < 0){
@@ -46,8 +49,36 @@ public class PlayerLogic : MonoBehaviour {
 			anim.SetInteger("PlayerDirection", 2);
 		} else {
 			anim.SetInteger("PlayerDirection", 0);
-		}
+		} */
 
 
+	}
+
+	void FixedUpdate () {
+			//rigidbody.AddForce(acceleration, 0f, 0f, ForceMode.Acceleration);
+
+			transform.Translate(Input.GetAxis("Horizontal") * PlayerSpeed * Time.deltaTime,0,0);
+			//transform.Translate(0,Input.GetAxis("Vertical") * PlayerSpeed * Time.deltaTime,0);
+			
+			transform.Translate(0,0,Input.GetAxis("Vertical") * PlayerSpeed * Time.deltaTime);
+
+            if (touchingPlatform &&Input.GetKey("space"))
+            {
+                rigidbody.AddForce(Vector3.up * jumpHigh, ForceMode.Force);
+
+			}
+
+            
+		
+
+
+	}
+
+	void OnCollisionEnter () {
+		touchingPlatform = true;
+	}
+	
+	void OnCollisionExit () {
+		touchingPlatform = false;
 	}
 }
